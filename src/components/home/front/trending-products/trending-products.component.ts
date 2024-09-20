8613cd084479c31f8b6f12c807ae1d27396a4deb
Alexander Quiroz <alexanderquiroz@info-mat.com>
2024-09-20 16:04:23 -0400

    last change move components

---
+++ src/components/home/front/trending-products/trending-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './trending-products.component.html',
+  styleUrl: './trending-products.component.css'
+})
+export class TrendingProductsComponent {
+
+}
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FeaturedProductComponent } from './front/featured-product/featured-product.component';
 import { DealsWeekComponent } from './front/deals-week/deals-week.component';
 import { TopSellingProductsComponent } from './front/top-selling-products/top-selling-products.component';
+import { TrendingProductsComponent } from './front/trending-products/trending-products.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FeaturedProductComponent,DealsWeekComponent,TopSellingProductsComponent],
+  imports: [FeaturedProductComponent,DealsWeekComponent,TopSellingProductsComponent,TrendingProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
