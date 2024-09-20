7a7f43f98648285984e7c22f64402f3341fa9832
Alexander Quiroz <alexanderquiroz@info-mat.com>
2024-09-20 14:28:35 -0400

    las update

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppComponent {
+  title = 'front-lapieza';
+}
---
+++ src/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
